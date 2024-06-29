8aaab6bc88486bb8a0662fdd415e2d7cbb1af25f
Nitesh kumar gupta <67727076+Nitesh844127@users.noreply.github.com>
2024-06-29 11:22:17 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+   
+    var multipleCancelButton = new Choices('#removeChoiceBtn', {
+       removeItemButton: true,
+       maxItemCount:5,
+    //    searchResultLimit:5,    //Appear dropdown option size
+    //    renderChoiceLimit:5   //Dropdown option size
+     }); 
+    
+    
+});
